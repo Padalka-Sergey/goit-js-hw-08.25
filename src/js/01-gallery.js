7107193e911f791c79576d9bca5e966758ee26bc
Padalka-Sergey <106059712+Padalka-Sergey@users.noreply.github.com>
2025-01-23 17:15:25 +0200

    hw-js-08-01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,44 @@
+// Описан в документации
+import SimpleLightbox from 'simplelightbox';
+// Дополнительный импорт стилей
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const listGalleryRef = document.querySelector('.some-element');
+const markup = createGalleryItems(galleryItems);
+
+listGalleryRef.addEventListener('click', openModalImageHandler);
+listGalleryRef.innerHTML = markup;
+
+function createGalleryItems(items) {
+  return items
+    .map(
+      ({ preview, original, description }) => `
+  <li class="gallery__item">
+  <a class="gallery__link" href=${original}>
+  <img
+  class="gallery__image"
+  src=${preview}
+  alt=${description}
+  />
+  </a>
+  </li>`
+    )
+    .join('');
+}
+
+function openModalImageHandler(evt) {
+  evt.preventDefault();
+  if (evt.target.nodeName !== 'IMG') {
+    return;
+  }
+  new SimpleLightbox('.some-element a', {
+    // captions: true,
+    // captionPosition: 'top',
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+}
