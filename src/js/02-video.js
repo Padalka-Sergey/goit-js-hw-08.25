28be66eb00c7d4fe27c47e96abd0db1ac1aa9274
Padalka-Sergey <106059712+Padalka-Sergey@users.noreply.github.com>
2025-01-26 19:33:45 +0200

    hw-08-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,34 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+let currentTime = localStorage.getItem('videoplayer-current-time');
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+if (currentTime) {
+  player
+    .setCurrentTime(currentTime)
+    .then(function (seconds) {
+      console.log(`${seconds}  -  the actual time that the player seeked to`);
+    })
+    .catch(function (error) {
+      switch (error.name) {
+        case 'RangeError':
+          console.log(
+            'the time was less than 0 or greater than the video’s duration'
+          );
+          break;
+
+        default:
+          console.log('some other error occurred');
+          break;
+      }
+    });
+}
+
+function onTimeUpdate(data) {
+  const time = JSON.stringify(data.seconds);
+  localStorage.setItem('videoplayer-current-time', time);
+}
